327cadf9f694d5b3b739a20d3b01ba98ea3ba04b
Bharath <bharathrajrb20it@gmail.com>
2023-12-26 06:51:42 +0530

    new

---
+++ Java/spring boot/demo/demo/src/main/java/com/example/demo/demo1/doc.java
@@ -0,0 +1,8 @@
+package com.example.demo.demo1;
+
+public class doc {
+  void assist() {
+    System.out.println("assist");
+  }
+
+}
---
+++ Java/spring boot/demo/demo/src/main/java/com/example/demo/demo1/main.java
@@ -0,0 +1,8 @@
+package com.example.demo.demo1;
+
+public class main {
+  public static void main(String[] args) {
+    doc doctor = new doc();
+    doctor.assist();
+  }
+}
